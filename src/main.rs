aa1756e6906fab39e48545a93c1e2d39462ffb23
itsgerliz <gerworks.hs@gmail.com>
2024-09-12 20:28:33 +0200

    Finished numberguess

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello, world!");
+	println!("Welcome, guess a number between 1 and 100");
+	let rand_number: u8 = fastrand::u8(1..=100);
+	
+	loop {
+		let mut user_guess: String = std::string::String::new();
+
+		std::io::stdin()
+			.read_line(&mut user_guess)
+			.expect("Failed to read line");
+		
+		let user_guess: u8 = match user_guess.trim().parse() {
+			Ok(num) => num,
+			Err(_) => {
+				println!("Please enter a valid number");
+				continue;
+			}
+		};
+
+		if rand_number > user_guess {
+			println!("More");
+		} else if rand_number < user_guess {
+			println!("Less");
+		} else {
+			println!("Match");
+			break;
+		}
+	}
 }
