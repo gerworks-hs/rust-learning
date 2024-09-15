8bec732c047a98f69f9fc5d513f2e145b87ac91a
itsgerliz <gerworks.hs@gmail.com>
2024-09-15 22:20:35 +0200

    Finished base_converter

--- base_converter/src/main.rs
+++ base_converter/src/main.rs
@@ -1,29 +1,40 @@
 fn main() {
+
 	let argv: Vec<String> = std::env::args().collect::<Vec<String>>();
 	let argc: usize = argv.len();
 
-	if argc < 3 || argc > 3 {
-		println!("Usage: {:?} <from base> <to base>", &argv[0]);
+	if argc < 4 || argc > 4 {
+		println!("Usage: {:?} <from base> <to base> <number>", &argv[0]);
 		std::process::exit(1);
 	} else {
-		println!("From base {} to base {}", &argv[1], &argv[2]);
+		println!("Number <{}> from base <{}> to base <{}>", &argv[3], &argv[1], &argv[2]);
 	}
 
 	let from_base: u8 = argv[1].parse::<u8>().expect("Cannot convert first argument");
 	let to_base: u8 = argv[2].parse::<u8>().expect("Cannot convert second argument");
 
-	match from_base {
-		2 => {()},
-		10 => {()},
-		_ => {
-			panic!("Invalid first base, possible bases are 2 and 10");
+	match (from_base, to_base) {
+		(2, 10) => {
+			match usize::from_str_radix(&argv[3], 2) {
+				Ok(value) => {
+					println!("Binary <{}> is <{}> decimal", &argv[3], value);
+				}
+				Err(_) => {
+					println!("Cannot convert number");
+					std::process::exit(1);
+				}
+			}
 		}
-	}
-	match to_base {
-		2 => {()},
-		10 => {()},
+		(10, 2) => {
+			let number: usize = argv[3].trim().parse::<usize>().expect("Cannot convert number");
+			
+			let binary: String = format!("{:b}", number);
+
+			println!("Decimal <{}> equals to <{}> binary", number, binary);
+		}
 		_ => {
-			panic!("Invalid second base, possible bases are 2 and 10");
+			println!("Invalid bases or they are the same");
+			std::process::exit(1);
 		}
 	}
 
